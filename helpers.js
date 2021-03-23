dec67b332ab4f6f47ca7e6c0d64c87dae499d04f
priya521 <priya521.j@gmail.com>
2021-03-23 03:42:22 +0000

    updated helpers file to resolve the error

--- helpers.js
+++ helpers.js
@@ -1,17 +1,46 @@
-
-const emailLooker = function (email, database) {
-  for (let id in database) {
-      if (database[id].email === email) {
-          return id
-      }
+// emailLookup - uses a user's email to find and return that user's object with the userDatabase.
+const emailLookup = function(email, database) {
+  for (let user in database) {
+    if (email === database[user].email) {
+      return database[user];
+    }
   }
-}
+  return false;
+};
 
-//generates a random string
-const generateRandomString = function () {
-  let randNum = Math.floor((Math.random() * 1000 + 10000));
-  let randString = 'a' + randNum;
-  return randString;
-}
+// generateRandomString - generates a random 6-digit alphanumeric string, for URL and cookie generation.
+const generateRandomString = function() {
+  let alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let output = "";
+  for (let i = 0; i < 6; i++) {
+    output += alphabet[Math.floor(Math.random() * alphabet.length)];
+  }
+  return output;
+};
 
-module.exports = { emailLooker, generateRandomString };
+
+
+// httpify - used to ensure all URLs have the http://www. or https://www. prefix
+const httpify = function(link) {
+  let output = link;
+  if (output.substring(0, 4) === "www.") {
+    output = "http://" + link;
+  }
+  if (output.substring(0, 11) !== "http://www." && output.substring(0, 12) !== "https://www.") {
+    output = "http://www." + link;
+  }
+  return output;
+};
+
+// urlsForUser - used to scan the urlDatabase and returns all URLs owned by the id in an object
+const userURLs = function(id, database) {
+  let output = {};
+  for (let url in database) {
+    if (id === database[url].userID) {
+      output[url] = database[url];
+    }
+  }
+  return output;
+};
+
+module.exports = { emailLookup, generateRandomString,  httpify, userURLs };
